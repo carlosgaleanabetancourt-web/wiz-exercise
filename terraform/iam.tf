3d104d0c8d783e8d8db8aaa00d26035c1673cdc1
carlosgaleanabetancourt-web <carlos.galeanabetancourt@gmail.com>
2026-09-08 15:03:48 -0600

    Update OIDC thumbprint for GitHub Actions

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -123,7 +123,10 @@ resource "aws_iam_role_policy" "mongodb_ec2_access" {
 resource "aws_iam_openid_connect_provider" "github_actions" {
   url = "https://token.actions.githubusercontent.com"
   client_id_list = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+  thumbprint_list = [
+    "1c58a3a8518e8759bf075b76b750d4f2df264fcd",
+    "6938fd4d98bab03faadb97b34396831e3780aea1"
+  ]
 }
 
 resource "aws_iam_role" "github_actions" {
