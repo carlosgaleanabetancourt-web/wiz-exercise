6f8ebbdfee6ba5480af70acab5f1a5eb957dbc7a
carlosgaleanabetancourt-web <carlos.galeanabetancourt@gmail.com>
2026-09-14 08:52:22 -0600

    fix: replace external Unicons CDN with self-contained CSS icons
    The Unicons CDN (unicons.iconscout.com) is blocked by corporate proxies,
    causing edit/delete icons to not render. Replace with CSS-only Unicode
    characters that require no external resources.
    
    - Remove Unicons CDN link from todo.html
    - Add .uil-pen and .uil-trash CSS rules using Unicode glyphs
    - Remove unicons.iconscout.com from CSP (style-src and font-src)
    - Add 'self' to font-src for any future local fonts

--- app/main.go
+++ app/main.go
@@ -23,8 +23,8 @@ func securityHeaders() gin.HandlerFunc {
 		c.Header("Content-Security-Policy",
 			"default-src 'self'; "+
 				"script-src 'self' 'unsafe-inline'; "+
-				"style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://unicons.iconscout.com; "+
-				"font-src https://fonts.gstatic.com https://unicons.iconscout.com; "+
+				"style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; "+
+				"font-src 'self' https://fonts.gstatic.com; "+
 				"img-src 'self' data:; "+
 				"connect-src 'self'")
 		c.Next()
